1b208811b30b22c9f00a77591d1c31cc405dc191
vadim <vadim.angryl1on@gmail.com>
2025-05-14 20:11:09 +0300

    Update Reducer and ViewModel

--- core/common/src/main/kotlin/com/mariqzw/supportorganizationsapp/common/presentation/ViewModel.kt
+++ core/common/src/main/kotlin/com/mariqzw/supportorganizationsapp/common/presentation/ViewModel.kt
@@ -1,7 +1,8 @@
 package com.mariqzw.supportorganizationsapp.common.presentation
 
+import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.Flow
 
-abstract class ViewModel<T : UiState, in I : ModelIntent> {
+abstract class ViewModel<T : UiState, in I : ModelIntent> : ViewModel() {
     abstract val state: Flow<T>
 }
