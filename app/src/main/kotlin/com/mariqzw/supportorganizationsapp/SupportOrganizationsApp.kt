4a76aefe123e5ffb4dd514598c969ad954f34a97
Maria <masha.podrez@mail.ru>
2025-05-18 15:46:04 +0300

    Add modules in app

--- app/src/main/kotlin/com/mariqzw/supportorganizationsapp/SupportOrganizationsApp.kt
+++ app/src/main/kotlin/com/mariqzw/supportorganizationsapp/SupportOrganizationsApp.kt
@@ -1,10 +1,15 @@
 package com.mariqzw.supportorganizationsapp
 
 import android.app.Application
+import com.mariqzw.supportorganizationsapp.applications.di.provideApplicationsModule
+import com.mariqzw.supportorganizationsapp.auth.di.provideAuthModule
+import com.mariqzw.supportorganizationsapp.common.di.provideCoroutineScopesModule
+import com.mariqzw.supportorganizationsapp.common.di.provideDispatcherModule
+import com.mariqzw.supportorganizationsapp.data.di.provideDataStoreModule
 import com.mariqzw.supportorganizationsapp.di.provideNavigationModule
-import com.mariqzw.supportorganizationsapp.di.provideNetworkEndpointsModule
-import com.mariqzw.supportorganizationsapp.di.provideNetworkModule
 import com.mariqzw.supportorganizationsapp.di.provideViewModelModule
+import com.mariqzw.supportorganizationsapp.network.di.provideNetworkEndpointsModule
+import com.mariqzw.supportorganizationsapp.network.di.provideNetworkModule
 import com.mariqzw.supportorganizationsapp.reporting.ConsoleLoggingTree
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -25,8 +30,20 @@ class SupportOrganizationsApp : Application() {
             // :app modules
             modules(provideNavigationModule, provideViewModelModule)
 
+            // core:common modules
+            modules(provideCoroutineScopesModule, provideDispatcherModule)
+
+            // core:data modules
+            modules(provideDataStoreModule)
+
             // core:network modules
             modules(provideNetworkModule, provideNetworkEndpointsModule)
+
+            // feature:ui-auth modules
+            modules(provideAuthModule)
+
+            // feature:ui-applications modules
+            modules(provideApplicationsModule)
         }
     }
 }
