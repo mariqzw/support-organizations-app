c70bc188d0bb690ea2cdd996f1bbc633670af59f
Maria <masha.podrez@mail.ru>
2025-05-18 13:58:11 +0300

    Fix ViewModel and Reducer

--- core/common/src/main/kotlin/com/mariqzw/supportorganizationsapp/common/presentation/Reducer.kt
+++ core/common/src/main/kotlin/com/mariqzw/supportorganizationsapp/common/presentation/Reducer.kt
@@ -1,4 +1,21 @@
 package com.mariqzw.supportorganizationsapp.common.presentation
 
-class Reducer {
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+abstract class Reducer<S : UiState, I : ModelIntent>(initialVal: S) {
+
+    private val _state: MutableStateFlow<S> = MutableStateFlow(initialVal)
+    val state: StateFlow<S> get() = _state.asStateFlow()
+
+    fun sendIntent(intent: I) {
+        reduce(_state.value, intent)
+    }
+
+    fun setState(newState: S) {
+        _state.tryEmit(newState)
+    }
+
+    abstract fun reduce(oldState: S, intent: I)
 }
