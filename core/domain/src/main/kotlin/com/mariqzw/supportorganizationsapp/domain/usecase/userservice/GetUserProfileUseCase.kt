a0622707d727c91f089f58095f32f87ba1027948
Maria <masha.podrez@mail.ru>
2025-05-19 22:20:31 +0300

    Update usecase with correct token usage

--- core/domain/src/main/kotlin/com/mariqzw/supportorganizationsapp/domain/usecase/userservice/GetUserProfileUseCase.kt
+++ core/domain/src/main/kotlin/com/mariqzw/supportorganizationsapp/domain/usecase/userservice/GetUserProfileUseCase.kt
@@ -3,5 +3,5 @@ package com.mariqzw.supportorganizationsapp.domain.usecase.userservice
 import com.mariqzw.supportorganizationsapp.model.response.UserResponse
 
 interface GetUserProfileUseCase {
-    suspend operator fun invoke(token: String): Result<UserResponse>
+    suspend operator fun invoke(): Result<UserResponse>
 }
--- core/network/src/main/kotlin/com/mariqzw/supportorganizationsapp/network/userservice/usecase/GetUserProfileUseCaseImpl.kt
+++ core/network/src/main/kotlin/com/mariqzw/supportorganizationsapp/network/userservice/usecase/GetUserProfileUseCaseImpl.kt
@@ -2,11 +2,15 @@ package com.mariqzw.supportorganizationsapp.network.userservice.usecase
 
 import com.mariqzw.supportorganizationsapp.domain.usecase.userservice.GetUserProfileUseCase
 import com.mariqzw.supportorganizationsapp.model.response.UserResponse
+import com.mariqzw.supportorganizationsapp.network.tokenservice.TokenSupport
 import com.mariqzw.supportorganizationsapp.network.userservice.UserService
 
 class GetUserProfileUseCaseImpl(
-    private val userService: UserService
+    private val userService: UserService,
+    private val tokenSupport: TokenSupport
 ) : GetUserProfileUseCase {
-    override suspend fun invoke(token: String): Result<UserResponse> =
-        userService.getUserProfile(token)
+    override suspend fun invoke(): Result<UserResponse> =
+        tokenSupport.withTokenCheck { token ->
+            userService.getUserProfile(token)
+        }
 }
